4f73d654719858f0812e555ae03d2c1ce7170914
Erik Savage <erikmsavage@gmail.com>
2024-04-02 20:13:45 -0400

    Updating problem 35 with new solution

--- 35_search_insert_position.py
+++ 35_search_insert_position.py
@@ -1,6 +1,31 @@
 # See: https://leetcode.com/problems/search-insert-position/
 class Solution(object):
     def searchInsert(self, nums, target):
+        return self.soln2(nums, target)
+        # return self.soln1(nums, target)
+    
+    # Same approach but without recursion
+    def soln2(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+        length = end - start + 1
+        while length > 2:
+            middle = (start + end) // 2
+            if target == nums[middle]:
+                return middle
+            elif target < nums[middle]:
+                end = middle - 1
+            else:
+                start = middle + 1
+            length = end - start + 1
+
+        if target <= nums[start]:
+            return start
+        if target <= nums[end]:
+            return end
+        return end+1
+
+    def soln1(self, nums, target):
         return self.search(nums, target, 0, len(nums)-1)
     
     def search(self, nums, target, start, end):
