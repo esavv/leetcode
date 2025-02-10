75b57885678851a0a937cb8841afaa098935adf4
Erik Savage <erikmsavage@gmail.com>
2025-02-10 17:22:26 -0500

    Updating 2352 with optimal soln

--- problems/2352_equal_row_column_pairs.py
+++ problems/2352_equal_row_column_pairs.py
@@ -4,31 +4,42 @@ class Solution(object):
         return self.soln2(grid)
         # return self.soln1(grid)
         
+    # soln #2 from 2/10/2025
+    # hashmap
     def soln2(self, grid):
+        numPairs = 0
         n = len(grid)
-        res = 0
-        start = [0] * n
-        for x in range(n):
-            match = True
-            if start[x] == -1:
-                continue
-            for y in range(start[x], n):
-                if grid[x][y] == grid[y][x]:
-                    start[y] += 1
-                else:
-                    match = False
-                    start[y] = -1
-            res += match
-        return res
+        rows = {}
 
+        for i in range(n):
+            row = ''
+            for j in range(n):
+                row += str(grid[i][j]) + ','
+            if row in rows:
+                rows[row] += 1
+            else:
+                rows[row] = 1
+
+        for i in range(n):
+            col = ''
+            for j in range(n):
+                col += str(grid[j][i]) + ','
+            if col in rows:
+                numPairs += rows[col]
+        return numPairs
+
+    # soln #1 from 2/10/2025
+    # brute force
     def soln1(self, grid):
+        numPairs = 0
         n = len(grid)
-        res = 0
-        for x in range(n):
-            match = True
-            for y in range(n):
-                if grid[x][y] != grid[y][x]:
-                    match = False
-                    break
-            res += match
-        return res
+        for i in range(n):
+            for j in range(n):
+                areEqual = True
+                for k in range(n):
+                    if grid[i][k] != grid[k][j]:
+                        areEqual = False
+                        break
+                if areEqual:
+                    numPairs += 1
+        return numPairs
