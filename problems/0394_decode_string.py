5c8835ea446fe109368453a167fec61bd59d3e69
Erik Savage <erikmsavage@gmail.com>
2025-03-01 11:09:24 -0500

    Updating 394 with new attempt

--- problems/0394_decode_string.py
+++ problems/0394_decode_string.py
@@ -1,7 +1,30 @@
 # See: https://leetcode.com/problems/decode-string/
 class Solution(object):
     def decodeString(self, s):
-        return self.soln1(s)
+        return self.soln2(s)
+        # return self.soln1(s)
+
+    # soln #1 from 3/01/2025
+    # recursive approach
+    def soln2(self, s):
+        def decode(k, idx):
+            decoded = ''
+            while idx < len(s):
+                while idx < len(s) and ord('a') <= ord(s[idx]) <= ord('z'):
+                    decoded += s[idx]
+                    idx += 1
+                if idx < len(s) and s[idx] == ']':
+                    idx += 1
+                    break
+                num = ''
+                while idx < len(s) and ord('0') <= ord(s[idx]) <= ord('9'):
+                    num += s[idx]
+                    idx += 1
+                if len(num) > 0:
+                    substr, idx = decode(int(num), idx+1)
+                    decoded += substr
+            return decoded * k, idx
+        return decode(1, 0)[0]
 
     # recursive approach. n = input string length, k = # of expansions required
     def soln1(self, s):
