10d29eef93770c923c174ccf3af9b46e3bb21028
Erik Savage <erikmsavage@gmail.com>
2025-03-09 18:35:29 -0400

    Updating 1584 with pimms algo

--- problems/1584_min_cost_to_connect_all_points.py
+++ problems/1584_min_cost_to_connect_all_points.py
@@ -1,7 +1,41 @@
 # See: https://leetcode.com/problems/min-cost-to-connect-all-points/
 class Solution(object):
     def minCostConnectPoints(self, points):
-        return self.soln1(points)
+        return self.soln2(points)
+        # return self.soln1(points)
+
+    # soln #1 from 3/08/2025
+    # prim's algorithm
+    def soln2(self, points):
+        n = len(points)
+        graph = [[0] * n for _ in range(n)]
+        for i in range(n-1):
+            for j in range(i+1, n):
+                weight = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                graph[i][j] = graph[j][i] = weight
+
+        unvisited = set([i for i in range(1, n)])
+        cost = 0
+
+        heap = []
+        for point in unvisited:
+            weight = graph[0][point]
+            heap.append([weight, point])
+        import heapq
+        heapq.heapify(heap)
+
+        while unvisited:
+            weight, pointA = heapq.heappop(heap)
+            while pointA not in unvisited:
+                weight, pointA = heapq.heappop(heap)
+
+            cost += weight
+            unvisited.remove(pointA)
+
+            for pointB in unvisited:
+                weight = graph[pointA][pointB]
+                heapq.heappush(heap, [weight, pointB])
+        return cost
 
     # soln #1 from 2/28/2025
     # kruskal's algorithm with optimized union-find
