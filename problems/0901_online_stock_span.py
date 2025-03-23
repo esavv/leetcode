2e5fb0ea35a9cce6640a4e8ff80575f9ebb4e64d
Erik Savage <erikmsavage@gmail.com>
2025-03-24 07:32:16 +1100

    Updating 901 with latest attempt

--- problems/0901_online_stock_span.py
+++ problems/0901_online_stock_span.py
@@ -2,6 +2,10 @@
 class StockSpanner(object):
 
     def __init__(self):
+        # soln3 init:
+        # self.history = []
+        # self.max = float("-inf")
+
         # soln2 init:
         self.peaks = [] # stack of tuples: (peak price, peak span)
 
@@ -13,8 +17,26 @@ class StockSpanner(object):
         :type price: int
         :rtype: int
         """
-        return self.soln2(price)
+        return self.soln3(price)
+        # return self.soln2(price)
         # return self.soln1(price)
+
+    # soln #1 from 3/22/2025
+    # history with span skipping
+    def soln3(self, price):
+        if price >= self.max:
+            span = len(self.history) + 1
+            self.history.append([price, span])
+            self.max = price
+            return span
+
+        span = 1
+        idx = len(self.history) - 1
+        while idx >= 0 and price >= self.history[idx][0]:
+            span += self.history[idx][1]
+            idx -= self.history[idx][1]
+        self.history.append([price, span])
+        return span
 
     # stack optimization. still n^2 time in the worst case, I think, but much better avg. case performance
     def soln2(self, price):
