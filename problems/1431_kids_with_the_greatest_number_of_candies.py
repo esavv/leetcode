3b529d81e83c9f632d196074b1a2c842de96b690
Erik Savage <erikmsavage@gmail.com>
2025-04-10 11:45:10 -0400

    Updating 1431 with latest attempt

--- problems/1431_kids_with_the_greatest_number_of_candies.py
+++ problems/1431_kids_with_the_greatest_number_of_candies.py
@@ -1,6 +1,26 @@
 # See: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
+        return self.soln2(candies, extraCandies)
+        # return self.soln1(candies, extraCandies)
+
+    # soln #1 from 4/10/2025
+    def soln2(self, candies, extraCandies):
+        n = len(candies)
+        result = [False] * n
+
+        maxCandies = float("-inf")
+        for count in candies:
+            maxCandies = max(count, maxCandies)
+
+        for i in range(n):
+            if candies[i] + extraCandies >= maxCandies:
+                result[i] = True
+
+        return result
+
+    # soln #1 from 2/06/2024
+    def soln1(self, candies, extraCandies):
         maxc = 0
         for c in candies:
             maxc = max(c, maxc)
