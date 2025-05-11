16eeb093165d0071916e2ee01ab4de8ee5b8066b
Erik Savage <erikmsavage@gmail.com>
2025-05-11 16:25:00 -0400

    Update 93 with new solution

--- problems/0093_restore_ip_addresses.py
+++ problems/0093_restore_ip_addresses.py
@@ -1,8 +1,36 @@
 # See: https://leetcode.com/problems/restore-ip-addresses/
 class Solution(object):
     def restoreIpAddresses(self, s):
-        return self.soln1(s)
+        return self.soln2(s)
+        # return self.soln1(s)
         
+    # soln #2 on 5/11/2025
+    # backtracking
+    def soln2(self, s):
+        validIPs = []
+        n = len(s)
+        
+        def backtrack(dots, startIdx, addr):
+            if dots == -1:
+                validIPs.append(''.join(addr))
+                return
+            if dots > 0:
+                numRange = range(startIdx, min(startIdx+3, n - dots))
+                pad = '.'
+            else:
+                numRange = range(n-1, n)
+                pad = ''
+            for i in numRange:
+                currNum = s[startIdx:i+1]
+                if currNum == '0' or (currNum[0] != '0' and 0 <= int(currNum) <= 255):
+                    addr.append(currNum + pad)
+                    backtrack(dots-1, i+1, addr)
+                    addr.pop()
+            return
+
+        backtrack(3, 0, [])
+        return validIPs 
+
     # soln #1 on 5/11/2025
     # recursive DFS
     def soln1(self, s):
