ebcbc52719026aa5e79a9b0c26569ea61c1f01e6
Erik Savage <erikmsavage@gmail.com>
2025-05-26 13:24:34 -0400

    Updating LC with new attempt

--- problems/0075_sort_colors.py
+++ problems/0075_sort_colors.py
@@ -1,8 +1,24 @@
 # See: https://leetcode.com/problems/sort-colors/
 class Solution(object):
     def sortColors(self, nums):
-        return self.soln1(nums)
+        return self.soln2(nums)
+        # return self.soln1(nums)
         
+    # attempt with a LC hint
+    def soln2(self, nums):
+        counts = [0] * 3
+        for val in nums:
+            counts[val] += 1
+
+        i = j = 0
+        while i < len(nums):
+            while counts[j] > 0:
+                nums[i] = j
+                counts[j] -= 1
+                i += 1
+            j += 1
+        return
+
     # merge sort attempt, didn't work
     def soln1(self, nums):
         n = len(nums)
